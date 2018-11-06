d84b16f71552bcfcf6ca7240d86d2404cc39df3a
Albert Janse van Rensburg <albert.jvrens@gmail.com>
2018-11-06 07:45:34 +0200

    Insertion and shell sort

---
+++ Sorting/InsertionSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, n;
+            var intArray = new int[20];
+            Console.WriteLine("Enter the number of elements : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write($"Enter element {i + 1} : ");
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            InsertionSort(intArray, n);
+
+            Console.WriteLine("The sorted array is : ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write($"{intArray[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static void InsertionSort(int[] arrayToSort, int n)
+        {
+            int i, j, temp;
+            for (i = 0; i < n; i++)
+            {
+                temp = arrayToSort[i];
+
+                for (j = i - 1; j >= 0 && arrayToSort[j] > temp; j--)
+                {
+                    arrayToSort[j + 1] = arrayToSort[j];
+                }
+
+                arrayToSort[j + 1] = temp;
+            }
+        }
+    }
+}
---
+++ Sorting/ShellSort/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, n;
+            var intArray = new int[20];
+            Console.WriteLine("Enter the number of elements : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write($"Enter element {i + 1} : ");
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            ShellSort(intArray, n);
+
+            Console.WriteLine("The sorted array is : ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write($"{intArray[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static void ShellSort(int[] arrayToSort, int n)
+        {
+            int i, j, temp, h;
+
+            Console.Write("Enter maximum increment(odd value) : ");
+            h = Convert.ToInt32(Console.ReadLine());
+
+            while (h >= 1)
+            {
+                for (i = h; i < n; i++)
+                {
+                    temp = arrayToSort[i];
+                    for (j = i - h; j >= 0 && arrayToSort[j] > temp; j = j - h)
+                    {
+                        arrayToSort[j + h] = arrayToSort[j];
+                    }
+
+                    arrayToSort[j + h] = temp;
+                }
+
+                h = h - 2;
+            }
+        }
+    }
+}
